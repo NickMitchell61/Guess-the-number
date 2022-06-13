5846765adfdad1afb0dc1c8a9d94bbc192e4f278
Maksym Nemera <Maksym.nemera@outlook.com>
2022-06-13 15:55:24 +0300

    Day2. Add replay in the game

--- logic.py
+++ logic.py
@@ -6,48 +6,65 @@ import time
 def anytext(text):
     '''Makes all game text typed'''
     for i in str(text):
-        time.sleep(0)
+        time.sleep(0.04)
         print(i, end='', flush=True)
 
 
-anytext('\nIn this game try to guess the number between 1 to max*')
+GAME = True
 
-MAX_NUMBER = 0
+while GAME:
+    anytext('\nIn this game try to guess the number between 1 to max*')
 
-while MAX_NUMBER == 0:
-    try:
-        anytext(MAX_NUMBER := int(input('\nEnter the max number or press the Enter to max is 50: ') or '50'))
-    except ValueError as ve:
-        anytext('\nError. You can enter only the number. Try again..')
+    MAX_NUMBER = 0
 
-RAND = random.randint(1, MAX_NUMBER)
+    while MAX_NUMBER == 0:
+        try:
+            anytext(MAX_NUMBER := int(input('\nEnter the max number or press the Enter to max is 50: ') or '50'))
+        except ValueError as ve:
+            anytext('\nError. You can enter only the number. Try again..')
 
-TRIES = 6
+    RAND = random.randint(1, MAX_NUMBER)
 
-anytext(f'\nOK, let\'s play. \nYou have an {TRIES} tries')
-anytext('\nWhat the number did I guess?: ')
+    TRIES = 6
 
-while TRIES > 0:
-    try:
-        answer = int(input())
+    anytext(f'\nOK, let\'s play. \nYou have an {TRIES} tries')
+    anytext('\nWhat the number did I guess?: ')
 
-        TRIES -= 1
+    while TRIES > 0:
+        try:
+            answer = int(input())
 
-        if answer > RAND:
-            anytext('\nYour number is bigger then I guess number')
-            anytext(f'\nYou have an {TRIES} tries')
-            anytext('\nEnter the next number: ')
-        elif answer < RAND:
-            anytext('\nYour number is lower then I guess')
-            anytext(f'\nYou have an {TRIES} tries')
-            anytext('\nEnter the next number: ')
-        else:
-            anytext('\nYou WIN!')
-            anytext(f'\nThe number what I guess is {RAND}')
+            TRIES -= 1
+
+            if answer > RAND:
+                anytext('\nYour number is bigger then I guess number')
+                anytext(f'\nYou have an {TRIES} tries')
+                anytext('\nEnter the next number: ')
+            elif answer < RAND:
+                anytext('\nYour number is lower then I guess')
+                anytext(f'\nYou have an {TRIES} tries')
+                anytext('\nEnter the next number: ')
+            else:
+                anytext('\nYou WIN!')
+                anytext(f'\nThe number what I guess is {RAND}')
+                GAME = False
+                break
+        except ValueError as ve:
+            anytext('\nError. You can enter only the number. Try again..')
+
+        if TRIES <= 0:
+            anytext('\n\nYou lose this game :(')
+            anytext(f'\nThe number what I guess is {RAND}\n')
+            GAME = False
             break
-    except ValueError as ve:
-        anytext('\nError. You can enter only the number. Try again..')
-        anytext('\nEnter the number: ')
-else:
-    anytext('\nYou lose this game :(')
-    anytext(f'\nThe number what I guess is {RAND}\n\n')
+
+    while GAME is False:
+        anytext(status := input('\n\nDo you want to play again? [y/n]: ').lower() or 'y')
+        if status == 'y':
+            GAME = True
+        elif status == 'n':
+            GAME = False
+            anytext('\nSee you soon. Bye!')
+            break
+        else:
+            anytext('\n You can enter only \'n\' or \'y\' ')
